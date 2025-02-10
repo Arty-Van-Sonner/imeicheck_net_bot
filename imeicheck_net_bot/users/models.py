67e0241b051953a7981e63409c01600eb3a5e3c1
Artem Savchenko <valter.199669@gmail.com>
2025-02-10 21:05:48 +0300

    Fix name of field for one of model

--- imeicheck_net_bot/users/models.py
+++ imeicheck_net_bot/users/models.py
@@ -6,7 +6,7 @@ class CustomUser(AbstractUser):
     '''
     
     '''
-    user_id = models.IntegerField(unique = True, name = 'user_id', db_column = 'user_id', null = True)
+    telegram_user_id = models.IntegerField(unique = True, name = 'telegram_user_id', db_column = 'telegram_user_id', null = True)
     access_opened = models.BooleanField(default = True, name = 'access_opened', db_column = 'access_opened')
     created = models.DateTimeField(auto_now_add = True, name = 'created', db_column = 'created')
     changed = models.DateTimeField(auto_now = True, name = 'changed', db_column = 'changed')
